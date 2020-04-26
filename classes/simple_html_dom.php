f0e202de3052c7f8716813ec620c74bb8f6eeb58
kagg-design <info@kagg.eu>
2020-04-26 19:26:05 +0300

    Prevent double definition of constants if simple_html_dom is used somewhere.

--- classes/simple_html_dom.php
+++ classes/simple_html_dom.php
@@ -23,29 +23,31 @@
 
 namespace KAGG\SimpleHTMLDOM;
 
-define('HDOM_TYPE_ELEMENT', 1);
-define('HDOM_TYPE_COMMENT', 2);
-define('HDOM_TYPE_TEXT', 3);
-define('HDOM_TYPE_ENDTAG', 4);
-define('HDOM_TYPE_ROOT', 5);
-define('HDOM_TYPE_UNKNOWN', 6);
-define('HDOM_QUOTE_DOUBLE', 0);
-define('HDOM_QUOTE_SINGLE', 1);
-define('HDOM_QUOTE_NO', 3);
-define('HDOM_INFO_BEGIN', 0);
-define('HDOM_INFO_END', 1);
-define('HDOM_INFO_QUOTE', 2);
-define('HDOM_INFO_SPACE', 3);
-define('HDOM_INFO_TEXT', 4);
-define('HDOM_INFO_INNER', 5);
-define('HDOM_INFO_OUTER', 6);
-define('HDOM_INFO_ENDSPACE', 7);
+if ( ! defined( 'HDOM_TYPE_ELEMENT' ) ) {
+	define( 'HDOM_TYPE_ELEMENT', 1 );
+	define( 'HDOM_TYPE_COMMENT', 2 );
+	define( 'HDOM_TYPE_TEXT', 3 );
+	define( 'HDOM_TYPE_ENDTAG', 4 );
+	define( 'HDOM_TYPE_ROOT', 5 );
+	define( 'HDOM_TYPE_UNKNOWN', 6 );
+	define( 'HDOM_QUOTE_DOUBLE', 0 );
+	define( 'HDOM_QUOTE_SINGLE', 1 );
+	define( 'HDOM_QUOTE_NO', 3 );
+	define( 'HDOM_INFO_BEGIN', 0 );
+	define( 'HDOM_INFO_END', 1 );
+	define( 'HDOM_INFO_QUOTE', 2 );
+	define( 'HDOM_INFO_SPACE', 3 );
+	define( 'HDOM_INFO_TEXT', 4 );
+	define( 'HDOM_INFO_INNER', 5 );
+	define( 'HDOM_INFO_OUTER', 6 );
+	define( 'HDOM_INFO_ENDSPACE', 7 );
 
-defined('DEFAULT_TARGET_CHARSET') || define('DEFAULT_TARGET_CHARSET', 'UTF-8');
-defined('DEFAULT_BR_TEXT') || define('DEFAULT_BR_TEXT', "\r\n");
-defined('DEFAULT_SPAN_TEXT') || define('DEFAULT_SPAN_TEXT', ' ');
-defined('MAX_FILE_SIZE') || define('MAX_FILE_SIZE', 600000);
-define('HDOM_SMARTY_AS_TEXT', 1);
+	defined( 'DEFAULT_TARGET_CHARSET' ) || define( 'DEFAULT_TARGET_CHARSET', 'UTF-8' );
+	defined( 'DEFAULT_BR_TEXT' ) || define( 'DEFAULT_BR_TEXT', "\r\n" );
+	defined( 'DEFAULT_SPAN_TEXT' ) || define( 'DEFAULT_SPAN_TEXT', ' ' );
+	defined( 'MAX_FILE_SIZE' ) || define( 'MAX_FILE_SIZE', 600000 );
+	define( 'HDOM_SMARTY_AS_TEXT', 1 );
+}
 
 function file_get_html(
 	$url,
@@ -565,7 +567,7 @@ class simple_html_dom_node
 		global $debug_object;
 		if (is_object($debug_object)) { $debug_object->debug_log_entry(1); }
 
-		list($tag, $id, $class, $attributes, $cmb) = $selector;
+		[$tag, $id, $class, $attributes, $cmb] = $selector;
 		$nodes = array();
 
 		if ($parent_cmd === ' ') { // Descendant Combinator
@@ -667,13 +669,13 @@ class simple_html_dom_node
 				&& is_array($attributes)
 				&& !empty($attributes)) {
 					foreach($attributes as $a) {
-						list (
+						[
 							$att_name,
 							$att_expr,
 							$att_val,
 							$att_inv,
 							$att_case_sensitivity
-						) = $a;
+						] = $a;
 
 						// Handle indexing attributes (i.e. "[2]")
 						/**
